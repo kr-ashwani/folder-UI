176df162511c5f17493555172f1c945820876fe2
Ashwani Kumar <ashwanikr160@gmail.com>
2022-03-27 05:13:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import FileStructure from "./components/FolderStructure/FolderStructure";
 
 function App() {
+  useEffect(() => {
+    function setVh() {
+      console.log("hi");
+      let root = document.querySelector(":root");
+      let vh = window.innerHeight * 0.01;
+      root.style.setProperty("--vh", `${vh}px`);
+    }
+    setVh();
+
+    window.addEventListener("resize", setVh);
+    return () => {
+      console.log("removed");
+      window.removeEventListener("resize", setVh);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Folder UI</h2>
+      <FileStructure />
     </div>
   );
 }
---
+++ src/components/FolderStructure/FolderStructure.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./FolderStructure.css";
+import data from "../../data/files.json";
+
+function FileStructure() {
+  function folderClick(e) {
+    e.currentTarget.classList.toggle("close");
+  }
+
+  function folderParsing(data) {
+    let folderContent = [];
+    data.map((elem, id) => {
+      if (elem.type === "folder") {
+        folderContent.push(
+          <div key={`${id}`} className="folder close" onClick={folderClick}>
+            <i className="fa-solid fa-folder"></i>
+            <i className="fa-solid fa-folder-open"></i>
+            <span>{elem.name}</span>
+          </div>
+        );
+        return folderContent.push(folderParsing(elem.items));
+      } else
+        return folderContent.push(
+          <div className="file" key={`${id}`}>
+            <img
+              src={`https://pro.alchemdigital.com/api/extension-image/${elem.name
+                .split(".")
+                .pop()}`}
+              alt={`${elem.name.split(".")[0]}`}
+            />
+            <span>{elem.name}</span>
+          </div>
+        );
+    });
+    return folderContent;
+  }
+
+  function FolderDiv(arr, leftShift = 1) {
+    let arrCount = leftShift;
+
+    return arr.map((elem, id) =>
+      Array.isArray(elem) ? (
+        <div
+          key={`${id}+a`}
+          className={`leftShift=${arrCount}`}
+          style={{ paddingLeft: `${arrCount * 5}px` }}>
+          {FolderDiv(elem, arrCount + 1)}
+        </div>
+      ) : (
+        elem
+      )
+    );
+  }
+
+  return <div className="fileStructure">{FolderDiv(folderParsing(data))}</div>;
+}
+
+export default FileStructure;
