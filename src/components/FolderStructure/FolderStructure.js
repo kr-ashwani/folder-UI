4b272413ef0c713f72c41accc9941daaef4c7076
Ashwani Kumar <ashwanikr160@gmail.com>
2022-04-11 13:01:28 +0530

    final version

--- src/components/FolderStructure/FolderStructure.js
+++ src/components/FolderStructure/FolderStructure.js
@@ -1,66 +1,47 @@
-import React, { useEffect, useRef } from "react";
-import "./FolderStructure.css";
-import data from "../../data/files.json";
+import React, { useEffect } from 'react';
+import './FolderStructure.css';
+import data from '../../data/files.json';
 
 function FileStructure() {
-  const fileStruct = useRef();
-  function folderClick(e) {
-    e.currentTarget.classList.toggle("close");
-  }
   useEffect(() => {
-    fileStruct.current.childNodes[0].classList.toggle("close");
+    document
+      .querySelector('.fileStructure>.folder>.folderName')
+      .classList.toggle('close');
   }, []);
 
   function folderParsing(data) {
-    let folderContent = [];
-    data.map((elem, id) => {
-      if (elem.type === "folder") {
-        folderContent.push(
-          <div key={`${id}`} className="folder close" onClick={folderClick}>
-            <i className="fa-solid fa-folder"></i>
-            <i className="fa-solid fa-folder-open"></i>
-            <span>{elem.name}</span>
-          </div>
-        );
-        return folderContent.push(folderParsing(elem.items));
-      } else
-        return folderContent.push(
-          <div className="file" key={`${id}`}>
+    const folderDiv = [];
+    data.forEach((elem, id) => {
+      if (elem.type === 'file')
+        folderDiv.push(
+          <div className="file" key={id}>
             <img
               src={`https://pro.alchemdigital.com/api/extension-image/${elem.name
-                .split(".")
+                .split('.')
                 .pop()}`}
-              alt={`${elem.name.split(".")[0]}`}
+              alt={`${elem.name.split('.')[0]}`}
             />
             <span>{elem.name}</span>
           </div>
         );
+      else
+        folderDiv.push(
+          <div className="folder" key={id}>
+            <div
+              className="folderName close"
+              onClick={(e) => e.currentTarget.classList.toggle('close')}>
+              <i className="fa-solid fa-folder"></i>
+              <i className="fa-solid fa-folder-open"></i>
+              <span>{elem.name}</span>
+            </div>
+            <div className="folderItems">{folderParsing(elem.items)}</div>
+          </div>
+        );
     });
-    return folderContent;
+    return folderDiv;
   }
 
-  function FolderDiv(arr, leftShift = 1) {
-    let arrCount = leftShift;
-
-    return arr.map((elem, id) =>
-      Array.isArray(elem) ? (
-        <div
-          key={`${id}+a`}
-          className={`leftShift=${arrCount}`}
-          style={{ paddingLeft: `${15}px` }}>
-          {FolderDiv(elem, arrCount + 1)}
-        </div>
-      ) : (
-        elem
-      )
-    );
-  }
-
-  return (
-    <div ref={fileStruct} className="fileStructure">
-      {FolderDiv(folderParsing(data))}
-    </div>
-  );
+  return <div className="fileStructure">{folderParsing(data)}</div>;
 }
 
 export default FileStructure;
